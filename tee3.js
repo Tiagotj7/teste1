7d808384cddbed51e54c4356a474192340e9bc76
Tiagotj7 <tiagowork07@gmail.com>
2023-06-22 23:47:42 -0300

    atualizando2

--- tee3.js
+++ tee3.js
@@ -1,2 +1,26 @@
 alert("Você sabe onde esta?")
 alert("...")
+
+// Exibe a tela de espera
+function showLoading() {
+    document.getElementById('loading').style.display = 'flex';
+  }
+  
+  // Oculta a tela de espera
+  function hideLoading() {
+    document.getElementById('loading').style.display = 'none';
+    var tempoEspera =3000;
+  }
+  
+  // Defina o tempo de espera em milissegundos (por exemplo, 3 segundos = 3000)
+  var tempoEspera = 3000;
+  
+  // Exemplo de uso
+  showLoading();
+  
+  // Aguarda o tempo de espera e, em seguida, oculta a tela de espera
+  setTimeout(function() {
+    hideLoading();
+  }, tempoEspera);
+  
+
